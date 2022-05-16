c9100122380fc1e7dc8ae30d099dc7772c86f4e8
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-05-17 01:56:19 +0800

    Update formatting

--- src/main/kotlin/com/rest/practiceapp/service/AccountService.kt
+++ src/main/kotlin/com/rest/practiceapp/service/AccountService.kt
@@ -2,14 +2,11 @@ package com.rest.practiceapp.service
 
 import com.rest.practiceapp.model.Account
 import com.rest.practiceapp.dao.AccountRepository
-//import com.rest.practiceapp.dao.SaltRepository
 import com.rest.practiceapp.model.parameters.LoginParameters
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-//import com.rest.practiceapp.service.encrypt.PasswordService
-//import com.rest.practiceapp.service.encrypt.SaltService
 
 @Service
 class AccountService {
@@ -17,34 +14,33 @@ class AccountService {
     @Autowired
     lateinit var accountRepository: AccountRepository
 
-    fun signup(account: Account) : ResponseEntity<String>
-    {
-        return if(accountRepository.existsByEmail(account.email)) ResponseEntity<String>("Email already exist",HttpStatus.OK) else {
+    fun signup(account: Account): ResponseEntity<String> {
+        return if (accountRepository.existsByEmail(account.email)) ResponseEntity<String>(
+            "Email already exist",
+            HttpStatus.OK
+        ) else {
 
             accountRepository.save(account)
 
-            ResponseEntity<String>("Account Created Successfully",HttpStatus.CREATED)
+            ResponseEntity<String>("Account Created Successfully", HttpStatus.CREATED)
         }
     }
 
     fun login(loginParameters: LoginParameters): ResponseEntity<Any> {
         val emailExist = accountRepository.existsByEmail(loginParameters.email)
 
-        if(emailExist)
-        {
+        if (emailExist) {
             val account = accountRepository.findByEmail(loginParameters.email)
-            if(account.password == loginParameters.password)
-            {
+            if (account.password == loginParameters.password) {
                 return ResponseEntity.ok().body(account)
             }
         }
         return ResponseEntity.ok().body("Invalid User/Password")
     }
 
-    fun accounts(): ResponseEntity<List<Account>>{
+    fun accounts(): ResponseEntity<List<Account>> {
         val account = accountRepository.findAll()
-        return ResponseEntity<List<Account>>(account,HttpStatus.OK)
+        return ResponseEntity<List<Account>>(account, HttpStatus.OK)
     }
-
 
 }
